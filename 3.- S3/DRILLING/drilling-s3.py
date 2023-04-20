d4dd00eef2e990dbbd392812ae7855d5b8f580da
Eduardo Hurtado <contacto@eduardohurtado.info>
2023-04-20 19:16:03 -0400

    Evaluaciones OK

---
+++ 3.- S3/DRILLING/drilling-s3.py
@@ -0,0 +1,12 @@
+# Importando la libreria math
+import math 
+
+# Se asigna el valor a la variable y
+y = 81
+
+# Se realiza el calculo y asigna el valor a la variable
+raiz = math.sqrt(y)
+
+# Se imprime por pantalla la variable
+
+print(f"La raíz cuadrada es {raiz}")
---
+++ 3.- S3/REBOUND/rebound.py
@@ -0,0 +1,9 @@
+# Se definen las variables de forma interna
+a = 20
+b = 30
+
+# Se imprime el resultado de la operación directamente
+
+print("El resultado de la operación es " + str(a*b))
+
+print(f"El resultado es {a*b}")
