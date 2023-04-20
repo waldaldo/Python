02699806c44388d865b7c2129620a0f3fb6db896
Eduardo Hurtado <contacto@eduardohurtado.info>
2023-04-19 20:35:26 -0400

    primera sesión

---
+++ 3.- S3/helloworld.py
@@ -0,0 +1,11 @@
+#Comentario en una linea
+
+"""
+Comentario en bloque
+de varias lineas, con 3 comillas dobles o simples al inicio y al final
+"""
+# La indentiacion den python es de 4 espacios o 1 tabulador
+
+print("Hello world")
+
+
---
+++ 3.- S3/modulos.py
@@ -0,0 +1,6 @@
+from math import pi as PI_VALUE
+import math
+
+print(PI_VALUE)
+
+print(int(math.sqrt(25)))
