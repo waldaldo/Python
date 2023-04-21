7a27c5c3c0b3cbbe1f6d524073e688e485b7a5ba
Eduardo Hurtado <contacto@eduardohurtado.info>
2023-04-20 20:15:30 -0400

    actualziando

---
+++ 4.- S4/scopeVariables.py
@@ -0,0 +1,18 @@
+# Scope o alcance de una variable
+
+# alcance global, variables que se encuentran fuera de una estructura
+variable_global = "Se puede acceder a una variable global en todo el codigo"
+
+# Alcance local, están dentro de una estructura como una función
+#def funcion_local(parametros_entrada):
+def suma(a,b):
+    suma_valores = a + b # suma es una variable local
+    variable_global = suma_valores # Aqui sele asigna suma a la variable global
+    return suma_valores  # despues de la función, suma no se puede acceder
+
+print(suma(1,2)) # devuelve 3 porque se accede como metodo
+
+print(variable_global) # devuelve "Se puede acceder a una variable global en todo el codigo"
+
+#print(suma_valores) # Devuelve variable no definida porque no puede acceder a la variable local
+
---
+++ 4.- S4/variables.py
@@ -0,0 +1,18 @@
+# Tipos de datos
+# int
+# string
+# float
+# bool
+
+# Declaramos variables 
+# identificador = valor
+numero = 10 # variable con valor entero
+texto = "Hola Mundo" # variable con valor texto
+decimal = 10.5 # variable con valor decimal
+condicional = True # variable con valor condicional
+
+# no se declara el timpo de datos en las variables, cuando es un codigo largo se corre el peligro de castear la variable sin querer
+
+# LAs variables deben llevar un nombre descriptivo para una mejor definicion 
+
+print(numero)
