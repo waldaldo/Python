cd1d974f7555c6ba22cd8a4630a8dff33151c2c4
Eduardo Hurtado <contacto@eduardohurtado.info>
2023-05-03 22:17:43 -0400

    agregando try-except

--- CONSOLIDACION/adivinanza.py
+++ CONSOLIDACION/adivinanza.py
@@ -3,7 +3,6 @@ Simula un juego de adivinanza donde el jugador debe adivinar un numero aleatorio
 El programa le dará pistas al jugador sobre si el numero ingresado es mayor 
 o menor que el numero generado y seguirá pidiendo al jugador que ingrese un numero hasta que adivine correctamente
 """
-
 import random
 import os
 
@@ -17,8 +16,13 @@ tomaré un número y veamos si lo adivinas ;)
 
 """)
 
-numero1 = int(input("Ingresa tu primer número: "))
-numero2 = int(input("Ingresa tu segundo número: "))
+while True:
+    try:
+        numero1 = int(input("Ingresa tu primer número: "))
+        numero2 = int(input("Ingresa tu segundo número: "))
+        break
+    except ValueError:
+        print("Debes ingresar un número válido. Intenta de nuevo.")
 
 adivinar = 0
 intento = 0
@@ -31,24 +35,25 @@ else:
     menor = numero1
 
 numero_random = random.randint(menor, mayor)
-# print(numero_random)
-
 
 print(f""" 
 Lo tengo, debes adivinar un número entre {menor} y {mayor}
 """)
 
 while adivinar != numero_random:
-    adivinar = int(input('Ingresa tu número: '))
-    intento += 1
-    if adivinar >= menor and adivinar <= mayor:
-        if adivinar > numero_random:
-            print(
-                f"Tu numero es mayor al número a adivinar, llevas {intento} intento(s)")
-        elif adivinar < numero_random:
-            print(
-                f"Tu numero es menor al número a adivinar, llevas {intento} intento(s)")
+    try:
+        adivinar = int(input('Ingresa tu número: '))
+        if adivinar >= menor and adivinar <= mayor:
+            intento += 1
+            if adivinar > numero_random:
+                print(
+                    f"Tu numero es mayor al número a adivinar, llevas {intento} intento(s)")
+            elif adivinar < numero_random:
+                print(
+                    f"Tu numero es menor al número a adivinar, llevas {intento} intento(s)")
+            else:
+                print(f"Excelente! Adivinaste en tu intento número {intento}")
         else:
-            print(f"Excelente! Adivinaste en tu intento número {intento}")
-    else:
-        print(f"Ingresa un número entre {numero1} y {numero2}")
+            print(f"Ingresa un número entre {numero1} y {numero2}")
+    except ValueError:
+        print("Debes ingresar un número válido. Intenta de nuevo.")
